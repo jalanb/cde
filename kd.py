15693f1ad2e0922d7eb86971327523b067e5c481
J Alan Brogan <github@al-got-rhythm.net>
2013-09-05 07:49:59 +0100

    Separate timings to their own module

--- kd.py
+++ kd.py
@@ -35,7 +35,9 @@ import sys
 from fnmatch import fnmatch
 from optparse import OptionParser
 import csv
-import time
+
+
+import timings
 
 class ToDo(NotImplementedError):
 	"""Errors raised by this script"""
@@ -333,7 +335,7 @@ def add_path(old_paths, new_path):
 	Or update it if already present"""
 	is_new_path = True
 	result = []
-	new_time = time.time()
+	new_time = timings.now()
 	for old_rank, old_path, old_time in old_paths:
 		if new_path == old_path:
 			result.append((increment(old_rank), new_path, new_time))
@@ -362,35 +364,10 @@ def write_path(item):
 			csvfile.close()
 
 
-def time_since(atime):
-	"""Convert atime number of seconds to English
-
-	Retain only the two most significant numbers
-
-	>>> print time_since(time.time() - (13*60*60 + 2*60 + 5))
-	13 hours, 2 minutes
-	"""
-	interval = int(abs(float(atime)) - time.time())
-	interval = int(time.time() - float(atime))
-	minutes, seconds = divmod(interval, 60)
-	if not minutes:
-		return '%s seconds' % seconds
-	hours, minutes = divmod(minutes, 60)
-	if not hours:
-		return '%s minutes, %s seconds' % (minutes, seconds)
-	days, hours = divmod(hours, 24)
-	if not days:
-		return '%s hours, %s minutes' % (hours, minutes)
-	years, days = divmod(days, 365)
-	if not years:
-		return '%s days, %s hours' % (days, hours)
-	return '%s years, %s days' % (years, days)
-
-
 def list_paths():
 	paths = [p for p in read_paths()]
 	for order, (_rank, path, atime) in enumerate(reversed(paths)):
-		print '%3d: %r last used %s ago' % (order, path, time_since(atime))
+		print '%3d: %r last used %s ago' % (order, path, timings.time_since(atime))
 
 
 def show_path_to_item(item, prefixes):
