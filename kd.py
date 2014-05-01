bf14abec022d43dce26d06618665999da3079840
J Alan Brogan <github@al-got-rhythm.net>
2014-05-01 20:22:06 +0100

    Try to narrow down on prefixes

--- kd.py
+++ kd.py
@@ -621,6 +621,11 @@ def _find_in_paths(item, prefixes, paths):
             except IndexError:
                 raise RangeError(i, matched)
             return find_under_directory(result, prefixes)
+        elif len(matched) > 1:
+            found = [find_under_directory(m, prefixes) for m in matched]
+            found = [f for f in found if f]
+            if len(found) == 1:
+                return found[0]
         raise TryAgain('Too many possiblities\n\t%s' % as_menu_string(matched))
 
 
