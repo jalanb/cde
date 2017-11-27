2f3c95bdcbf70a0c93fda5a487ff97695c93409d
J Alan Brogan <github@al-got-rhythm.net>
2017-11-27 23:14:22 +0000

    Default to python2 on master branch

--- kd.py
+++ kd.py
@@ -487,7 +487,7 @@ def read_history():
     path = _path_to_history()
     if not path.isfile():
         return []
-    with open(path) as stream:
+    with open(path, 'r') as stream:
         reader = csv.reader(
             stream, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         return [_ for _ in reader if _]
