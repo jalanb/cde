3a8f8c10fd0677782e13d236a736db76f4b437d0
J Alan Brogan <github@al-got-rhythm.net>
2018-09-10 12:50:37 +0100

    Prefer python to manipluate string paths

--- cd.sh
+++ cd.sh
@@ -191,17 +191,32 @@ _here_ls () {
     ls 2> ~/bash/null
 }
 
+say_path () {
+    local _said=$(python << EOP
+# coding=utf8
+import os, sys
+path=os.path.expanduser(os.path.expandvars('$1'))
+home='%s/' % os.path.expanduser('~')
+if path.startswith(home):
+    out=path.replace(home, 'home ')
+elif path[0] == '/':
+    out='root %s' % path[1:]
+else:
+    out=path
+replacements = (
+    ('/wwts', '/dub dub t s'),
+    ('/䷃ ZatSo ?', '/is that so?'),
+)
+for old, new in replacements:
+    out = out.replace(old, new)
+sys.stdout.write(out.replace('/', ' '))
+EOP
+)
+    sai "$_said"
+}
+
 _post_cd () {
-    local _path=$(~/jab/bin/short_dir $PWD)
-    [[ $_path == "~" ]] && _path=HOME
-    [[ $_path =~ "wwts" ]] && _path="${_path/wwts/dub dub t s}"
-    local _tildless=${_path/~/home} # ; echo t $_tildless
-    local _homele=$(echo $_tildless | sed -e "s:$HOME:home:")
-    local _homeles=${_homele/home\//} # ; echo 1 $_homeles
-    local _homeless=${_homeles/home/} # ; echo 2 $_homeless
-    local _rootless=$(echo $_homeless | sed -e "s:^/:root :" )
-    local _said=$(echo $_rootless | sed -e "s:/: :g")
-    sai $_said
+    say_path $(short_dir "$PWD")
     _here_show_todo && echo
     _here_bash
     _here_bin
