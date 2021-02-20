01c7a5657078bff2670ec2913ad0b884598dbcbb
J Alan Brogan <github@al-got-rhythm.net>
2021-02-20 10:37:07 +0000

    Add Roots class to filter paths of children
    Takes out python dirs like .../cde/cde

--- cde/types.py
+++ cde/types.py
@@ -1,5 +1,6 @@
 """Types to make coding cde easier"""
 import os
+from typing import List
 
 
 from pysyte.types import paths
@@ -9,17 +10,34 @@ from pysyte.types.lists import UniquelyTrues
 class PossiblePaths(UniquelyTrues):
     """A unique list of possible paths"""
 
-    def predicate(self, item):
+    def convert(self, item: str) -> paths.StringPath:
+        return paths.path(item)
+
+    def predicate(self, item: str) -> bool:
         """Exclude items which don't exist"""
         return bool(item) and os.path.exists(item)
 
-    def paths(self):
-        return [paths.path(_) for _ in self if self.predicate(_)]
+    def paths(self) -> List[paths.StringPath]:
+        return [_ for _ in self if self.predicate(_)]
 
 
 class UniquePaths(PossiblePaths):
-    def contains(self, item):
+    def __contains__(self, item: ):
         for path in self:
             if path.same_path(item):
                 return True
         return False
+
+class Roots(UniquePaths):
+
+    def predicate(self, item: str) -> bool:
+        path_ = paths.path(item)
+        if path_ in self:
+            return False
+        if path_.parent in self:
+            return False
+        for i, root in enumerate(self):
+            if root.parent.same_path(path_):
+                break
+        else:
+            self[i] = path_
