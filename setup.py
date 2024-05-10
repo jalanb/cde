3a460a760778391f09655a2f78aaee0df809756a
J Alan Brogan <github@al-got-rhythm.net>
2024-05-10 01:18:18 +0100

    Bump version: 0.7.37 → 0.7.38

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="cde",
-    version="0.7.37",
+    version="0.7.38",
     url="https://github.com/jalanb/cde",
     license="MIT License",
     author="jalanb",
